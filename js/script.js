8a4a40d0c7f5307316011877d4b1252fe74b26ef
Nazar <youtopak@gmail.com>
2022-11-12 10:38:19 +0200

    lesson19_homework

--- js/script.js
+++ js/script.js
@@ -20,3 +20,22 @@ personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
 
+// homework
+
+const companyName = 'Fast Mechanism';
+
+const FastMechanismArchive = {
+    collaborators: ['Nazar', 'Maks', 'Nazar'],
+    asks: {},
+    employeesAreAccepted: false,
+};
+
+const e = prompt('чи ви хочетепрацювати в нашуй компанії?'),
+      f = prompt('чим ви будете корисний нашій компанії'),
+      g = prompt('як давго ви хочете працювати в нашуй компанії?'),
+      h = prompt('всім привіт');
+
+FastMechanismArchive.asks[e] = f;
+FastMechanismArchive.asks[g] = h;
+
+console.log(FastMechanismArchive);
