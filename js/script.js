d38917057a4b031bd6dc876d013160bffaab9d12
Nazar <youtopak@gmail.com>
2022-11-13 20:24:53 +0200

    add 23 lesson

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// "use strict";
+"use strict";
 
 // const numberOfFilms = +prompt("Скільки фільмів ви продивилися?", "");
 
@@ -42,59 +42,59 @@
 
 // // 20 урок
 
-// if (4==9) {
-//     console.log('Ok!');
-// }
+if (4==9) {
+    console.log('Ok!');
+}
 
-// if (4==4) {
-//     console.log('Ok!');
-// }
+if (4==4) {
+    console.log('Ok!');
+}
 
-// if (4==4) {
-//     console.log('Ok!');
-// } else {
+if (4==4) {
+    console.log('Ok!');
+} else {
+    console.log('Error!');
+}
+
+if (4==9) {
+    console.log('Ok!');
+} else {
+    console.log('Error!');
+}
+
+if (4) {
+    console.log('Ok!');
+} else {
+    console.log('Error!');
+}
+
+const num = 50;
+
+// if (num < 49) {
 //     console.log('Error!');
-// }
-
-// if (4==9) {
-//     console.log('Ok!');
+// } else if (num > 100) {
+//     console.log('много');
 // } else {
-//     console.log('Error!');
-// }
-
-// if (4) {
 //     console.log('Ok!');
-// } else {
-//     console.log('Error!');
 // }
 
-// const num = 50;
-
-// // if (num < 49) {
-// //     console.log('Error!');
-// // } else if (num > 100) {
-// //     console.log('много');
-// // } else {
-// //     console.log('Ok!');
-// // }
-
-// // (num === 50) ? console.log('Ok!') : console.log('Error!');
+// (num === 50) ? console.log('Ok!') : console.log('Error!');
 
 
-// switch (num) {
-//     case 49:
-//         console.log('неправильно');
-//         break;
-//     case 100:
-//         console.log('неправильно');
-//         break;
-//     case 50:
-//         console.log('в точку!');
-//         break;
-//     default:
-//         console.log('не в цей раз!');
-//         break;
-// }
+switch (num) {
+    case 49:
+        console.log('неправильно');
+        break;
+    case 100:
+        console.log('неправильно');
+        break;
+    case 50:
+        console.log('в точку!');
+        break;
+    default:
+        console.log('не в цей раз!');
+        break;
+}
 
 // // homework
 
@@ -125,136 +125,136 @@
 
 // 21 lesson
 
-// const hamburger = true,
-//       fries = true;
+// // const hamburger = true,
+// //       fries = true;
 
-// if (hamburger && fries) {
-//     console.log('я наївся');
-// } else {
-//     console.log('не має одного із інгриндієнтів');
-// }
+// // if (hamburger && fries) {
+// //     console.log('я наївся');
+// // } else {
+// //     console.log('не має одного із інгриндієнтів');
+// // }
 
-// const hamburger = true,
-//       fries = false;
+// // const hamburger = true,
+// //       fries = false;
 
-// if (hamburger && fries) {
-//     console.log('я наївся');
-// } else {
-//     console.log('не має одного із інгриндієнтів');
-// }
+// // if (hamburger && fries) {
+// //     console.log('я наївся');
+// // } else {
+// //     console.log('не має одного із інгриндієнтів');
+// // }
 
 
-// const hamburger = 5,
-//       fries = 0;
+// // const hamburger = 5,
+// //       fries = 0;
 
-// if (hamburger && fries) {
-//     console.log('я наївся');
-// } else {
-//     console.log('не має одного із інгриндієнтів');
-// }
+// // if (hamburger && fries) {
+// //     console.log('я наївся');
+// // } else {
+// //     console.log('не має одного із інгриндієнтів');
+// // }
 
-// console.log((hamburger && fries));
+// // console.log((hamburger && fries));
 
-// const hamburger = 5,
-//       fries = 5,
-//       cola = 3;
+// // const hamburger = 5,
+// //       fries = 5,
+// //       cola = 3;
 
-// console.log(hamburger > 3 && cola && fries);
+// // console.log(hamburger => 3 && cola && fries);
 
-// console.log(1 && 0);
-// console.log(1 && 5);
-// console.log(null && 5);
-// console.log(0 && 'gdlkfgkfd');
-// console.log(5 && 'ksljdfhlgk');
-// console.log(null && 'kdlsjfnklng');
-// console.log(null && 0 && 'dsafdeg');
+// // console.log(1 && 0);
+// // console.log(1 && 5);
+// // console.log(null && 5);
+// // console.log(0 && 'gdlkfgkfd');
+// // console.log(5 && 'ksljdfhlgk');
+// // console.log(null && 'kdlsjfnklng');
+// // console.log(null && 0 && 'dsafdeg');
 
-// if (hamburger > 3 && cola && fries) {
-//     console.log('ми наїлися');
-// } else {
-//     console.log('ми ідемо у вас немає того чого ми хочемо');
-// }
+// // if (hamburger > 3 && cola && fries) {
+// //     console.log('ми наїлися');
+// // } else {
+// //     console.log('ми ідемо у вас немає того чого ми хочемо');
+// // }
 
-// const hamburger = 5,
-//       fries = 0,
-//       cola = 0;
+// // const hamburger = 5,
+// //       fries = 0,
+// //       cola = 0;
 
-// if (hamburger || cola || fries) {
+// // if (hamburger || cola || fries) {
+// //     console.log('ми раді');
+// // } else {
+// //     console.log('ми ідемо у вас немає того чого ми хочемо');
+// // }
+
+// // const hamburger = 0,
+// //       fries = 0,
+// //       cola = 0;
+
+// // if (hamburger || cola || fries) {
+// //     console.log('ми раді');
+// // } else {
+// //     console.log('ми ідемо у вас немає того чого ми хочемо');
+// // }
+
+// // console.log(hamburger || cola || fries);
+
+// // const hamburger = 0,
+// //       fries = null,
+// //       cola = 0;
+
+// // if (hamburger || cola || fries) {
+// //     console.log('ми раді');
+// // } else {
+// //     console.log('ми ідемо у вас немає того чого ми хочемо');
+// // }
+
+// const hamburger = 3,
+//       fries = 3,
+//       cola = 0,
+//       nuggets = 2;
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
 //     console.log('ми раді');
 // } else {
 //     console.log('ми ідемо у вас немає того чого ми хочемо');
 // }
 
-// const hamburger = 0,
-//       fries = 0,
-//       cola = 0;
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
 
-// if (hamburger || cola || fries) {
-//     console.log('ми раді');
-// } else {
-//     console.log('ми ідемо у вас немає того чого ми хочемо');
-// }
+// let johnReport, alexReport, samReport, mariaReport = 'done';
 
-// console.log(hamburger || cola || fries);
+// console.log(johnReport || alexReport || samReport || mariaReport);
 
-// const hamburger = 0,
-//       fries = null,
-//       cola = 0;
+// console.log(!0);
 
-// if (hamburger || cola || fries) {
-//     console.log('ми раді');
-// } else {
-//     console.log('ми ідемо у вас немає того чого ми хочемо');
-// }
-
-const hamburger = 3,
-      fries = 3,
-      cola = 0,
-      nuggets = 2;
-
-if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-    console.log('ми раді');
-} else {
-    console.log('ми ідемо у вас немає того чого ми хочемо');
-}
-
-console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
-
-let johnReport, alexReport, samReport, mariaReport = 'done';
-
-console.log(johnReport || alexReport || samReport || mariaReport);
-
-console.log(!0);
-
-console.log(!1);
+// console.log(!1);
 
 
-// homework
+// // homework
 
-console.log( NaN || 2 || undefined );
+// console.log( NaN || 2 || undefined );
 
-console.log( NaN && 2 && undefined );
+// console.log( NaN && 2 && undefined );
 
-console.log( 1 && 2 && 3 );
+// console.log( 1 && 2 && 3 );
 
-console.log( !1 && 2 || !3 );
+// console.log( !1 && 2 || !3 );
 
-console.log( 25 || null && !3 );
+// console.log( 25 || null && !3 );
 
-console.log( NaN || null || !3 || undefined || 5);
+// console.log( NaN || null || !3 || undefined || 5);
 
-console.log( NaN || null && !3 && undefined || 5);
+// console.log( NaN || null && !3 && undefined || 5);
 
-console.log( 5 === 5 && 3 > 1 || 5);
+// console.log( 5 === 5 && 3 > 1 || 5);
 
-// const hamburger = 3;
-// const fries = 3;
-// const cola = 0;
-// const nuggets = 2;
+// // const hamburger = 3;
+// // const fries = 3;
+// // const cola = 0;
+// // const nuggets = 2;
  
-// if (hamburger === 3 && cola || fries === 3 && nuggets) {
-//    console.log('Done!')
-// }
+// // if (hamburger === 3 && cola || fries === 3 && nuggets) {
+// //    console.log('Done!')
+// // }
 
 // let hamburger;
 // const fries = NaN;
@@ -262,14 +262,102 @@ console.log( 5 === 5 && 3 > 1 || 5);
 // const nuggets = 2;
  
 // if (hamburger || cola || fries === 3 || nuggets) {
-//    console.log('Done!')
+//    console.log('Done!');
 // }
 
-// let hamburger;
-// const fries = NaN;
-// const cola = 0;
-// const nuggets = 2;
+// // let hamburger;
+// // const fries = NaN;
+// // const cola = 0;
+// // const nuggets = 2;
  
-// if (hamburger && cola || fries === 3 && nuggets) {
-//    console.log('Done!')
+// // if (hamburger && cola || fries === 3 && nuggets) {
+// //    console.log('Done!')
+// // }
+
+
+// lesson 22
+
+// let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// for (let i =1; i < 8; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(num);
+//     num++;
+// }
+
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         break;
+//         // continue;
+//     }
+    
+//     console.log(i);
+// }
+
+
+// lesson 23
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(j);
+//     }
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+// let result = '';
+// const lenght = 7;
+
+// for (let i = 1; i < lenght; i++) {
+
+//     for (let j = 0; j < i; j++) {
+//         result += '*';
+//     }
+
+//     result += '\n';
+// }
+
+// console.log(result);
+
+// first: for (let i = 0; i < 3; i++) {
+//     console.log(`first level: ${i}`);
+//     for (let j = 0; j < 4; j++) {
+//         console.log(`second level: ${j}`);
+//         for (let k = 0; k < 5; k++) {
+//             // if (k === 2) continue first;
+//             console.log(`third level: ${k}`);
+//         }
+//     }
+// }
+
+
+// first: for (let i = 0; i < 3; i++) {
+//     console.log(`first level: ${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`second level: ${j}`);
+//         for (let k = 0; k < 5; k++) {
+//             if (k === 2) break first;
+//             console.log(`third level: ${k}`);
+//         }
+//     }
 // }
